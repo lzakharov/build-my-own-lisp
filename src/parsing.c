cb983f719ca455921dcc8e8232e50d0fa7e43929
Lev Zakharov <l.j.zakharov@gmail.com>
2018-10-02 02:16:04 +0300

    Add Polish Notation Grammar evaluation

--- src/parsing.c
+++ src/parsing.c
@@ -1,6 +1,3 @@
-#include <stdio.h>
-#include <stdlib.h>
-
 #include "mpc.h"
 
 #ifdef _WIN32
@@ -25,6 +22,9 @@ void add_history(char* input) {}
 #include <editline/history.h>
 #endif
 
+long eval(mpc_ast_t*);
+long eval_op(long, char*, long);
+
 int main(int argc, char** argv) {
   mpc_parser_t* Number = mpc_new("number");
   mpc_parser_t* Operator = mpc_new("operator");
@@ -40,7 +40,7 @@ lispy    : /^/ <operator> <expr>+ /$/ ;				  \
 ",
 	    Number, Operator, Expr, Lispy);
   
-  puts("Lispy Version 0.0.0.0.1");
+  puts("Lispy Version 0.0.0.0.3");
   puts("Press Ctrl+C to Exit\n");
 
   while (1) {
@@ -49,7 +49,8 @@ lispy    : /^/ <operator> <expr>+ /$/ ;				  \
 
     mpc_result_t r;
     if (mpc_parse("<stdin>", input, Lispy, &r)) {
-      mpc_ast_print(r.output);
+      long result = eval(r.output);
+      printf("%li\n", result);
       mpc_ast_delete(r.output);
     } else {
       mpc_err_print(r.error);
@@ -60,5 +61,30 @@ lispy    : /^/ <operator> <expr>+ /$/ ;				  \
   }
 
   mpc_cleanup(4, Number, Operator, Expr, Lispy);
+  return 0;
+}
+
+long eval(mpc_ast_t* t) {
+  if (strstr(t->tag, "number")) {
+    return atoi(t->contents);
+  }
+
+  char* op = t->children[1]->contents;
+  long x = eval(t->children[2]);
+
+  int i = 3;
+  while (strstr(t->children[i]->tag, "expr")) {
+    x = eval_op(x, op, eval(t->children[i]));
+    ++i;
+  }
+
+  return x;
+}
+
+long eval_op(long x, char* op, long y) {
+  if (strcmp(op, "+") == 0) { return x + y; };
+  if (strcmp(op, "-") == 0) { return x - y; };
+  if (strcmp(op, "*") == 0) { return x * y; };
+  if (strcmp(op, "/") == 0) { return x / y; };
   return 0;
 }
