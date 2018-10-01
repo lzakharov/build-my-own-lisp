2ea7b3202ebdd75cec2c9610fb9cfd088328762f
Lev Zakharov <l.j.zakharov@gmail.com>
2018-10-02 00:35:08 +0300

    Add prompt portability

--- src/prompt.c
+++ src/prompt.c
@@ -1,7 +1,27 @@
 #include <stdio.h>
 #include <stdlib.h>
 
+#ifdef _WIN32
+#include <string.h>
+
+static char buffer[2048];
+
+char* readline(char* prompt) {
+  fputs(prompt, stdout);
+  fgets(buffer, 2048, stdin);
+  char* cpy = malloc(strlen(buffer)+1);
+  strcpy(cpy, buffer);
+  cpy[strlen(cpy)-1] = '\0';
+  return cpy;
+}
+
+void add_history(char* input) {}
+#elif __APPLE__
 #include <editline/readline.h>
+#elif __linux
+#include <editline/readline.h>
+#include <editline/history.h>
+#endif
 
 int main(int argc, char** argv) {
   puts("Lispy Version 0.0.0.0.1");
