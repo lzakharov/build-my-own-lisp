d3c9bbbd5629a23386c44e4846fd60f3ccdeb026
Lev Zakharov <l.j.zakharov@gmail.com>
2018-10-02 01:08:57 +0300

    Add Polish Notation Grammar parser

--- src/parsing.c
+++ src/parsing.c
@@ -26,6 +26,20 @@ void add_history(char* input) {}
 #endif
 
 int main(int argc, char** argv) {
+  mpc_parser_t* Number = mpc_new("number");
+  mpc_parser_t* Operator = mpc_new("operator");
+  mpc_parser_t* Expr = mpc_new("expr");
+  mpc_parser_t* Lispy = mpc_new("lispy");
+
+  mpca_lang(MPCA_LANG_DEFAULT,
+	    "                                                     \
+number   : /-?[0-9]+/ ;						  \
+operator : '+' | '-' | '*' | '/' ;				  \
+expr     : <number> | '(' <operator> <expr>+ ')' ;		  \
+lispy    : /^/ <operator> <expr>+ /$/ ;				  \
+",
+	    Number, Operator, Expr, Lispy);
+  
   puts("Lispy Version 0.0.0.0.1");
   puts("Press Ctrl+C to Exit\n");
 
@@ -36,5 +50,6 @@ int main(int argc, char** argv) {
     free(input);
   }
 
+  mpc_cleanup(4, Number, Operator, Expr, Lispy);
   return 0;
 }
