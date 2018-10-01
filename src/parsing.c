d4da623958e0edb5d0922c21d7c0bfdb0ecb22be
Lev Zakharov <l.j.zakharov@gmail.com>
2018-10-02 01:16:30 +0300

    Add user input parsing

--- src/parsing.c
+++ src/parsing.c
@@ -46,7 +46,16 @@ lispy    : /^/ <operator> <expr>+ /$/ ;				  \
   while (1) {
     char* input = readline("lispy> ");
     add_history(input);
-    printf("No you're a %s\n", input);
+
+    mpc_result_t r;
+    if (mpc_parse("<stdin>", input, Lispy, &r)) {
+      mpc_ast_print(r.output);
+      mpc_ast_delete(r.output);
+    } else {
+      mpc_err_print(r.error);
+      mpc_err_delete(r.error);
+    }
+    
     free(input);
   }
 
